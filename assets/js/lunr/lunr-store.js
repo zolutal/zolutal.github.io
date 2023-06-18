0592b889c66f755d1fe5e36a67523c6cc666506a
zolutal <zolutal@users.noreply.github.com>
2023-06-18 02:30:45 +0000

    deploy: 0c9b4abd0052f9e11ba182f96deabcf790d91d36

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Understanding Memory Deduplication Attacks",
+        "excerpt":"I’ve recently came across a bunch of research describing attacks on memory deduplication, it has been used to fingerprint systems[1], crack (K)ASLR[2,3,4], leak database records[4], and even exploit rowhammer[5]. It’s a really cool class of attacks that I hadn’t heard of before, but I wasn’t having much luck finding any...","categories": [],
+        "tags": ["Exploitation","Sidechannels","Linux","KVM","KASLR"],
+        "url": "/dedup-attacks/",
+        "teaser": null
+      }]
